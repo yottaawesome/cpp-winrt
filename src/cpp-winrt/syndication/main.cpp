498c1f1deecaba6a445e64dd23c3b99317a0d2f3
Vasilios Magriplis <etameson@gmail.com>
2023-03-10 20:19:28 +1100

    Minor changes

--- src/cpp-winrt/syndication/main.cpp
+++ src/cpp-winrt/syndication/main.cpp
@@ -5,24 +5,23 @@
 
 import <iostream>;
 
-using namespace winrt;
-using namespace Windows::Foundation;
-using namespace Windows::Web::Syndication;
-
 int main()
 {
     winrt::init_apartment();
 
-    Uri rssFeedUri{ L"https://blogs.windows.com/feed" };
-    SyndicationClient syndicationClient;
-    SyndicationFeed syndicationFeed = syndicationClient.RetrieveFeedAsync(rssFeedUri).get();
-    for (const SyndicationItem syndicationItem : syndicationFeed.Items())
+    winrt::Windows::Foundation::Uri rssFeedUri{ L"https://blogs.windows.com/feed" };
+    winrt::Windows::Web::Syndication::SyndicationClient syndicationClient;
+    winrt::Windows::Web::Syndication::SyndicationFeed syndicationFeed = syndicationClient.RetrieveFeedAsync(rssFeedUri).get();
+    for (const winrt::Windows::Web::Syndication::SyndicationItem syndicationItem : syndicationFeed.Items())
     {
         winrt::hstring titleAsHstring = syndicationItem.Title().Text();
 
         // A workaround to remove the trademark symbol from the title string, because it causes issues in this case.
         std::wstring titleAsStdWstring{ titleAsHstring.c_str() };
-        titleAsStdWstring.erase(remove(titleAsStdWstring.begin(), titleAsStdWstring.end(), L'™'), titleAsStdWstring.end());
+        titleAsStdWstring.erase(
+            remove(titleAsStdWstring.begin(), titleAsStdWstring.end(), L'™'), 
+            titleAsStdWstring.end()
+        );
         titleAsHstring = titleAsStdWstring;
 
         std::wcout << titleAsHstring.c_str() << std::endl;
