75bb3dac49306727620db3763300a46cccc02032
Vasilios Magriplis <etameson@gmail.com>
2023-03-10 19:59:27 +1100

    Add Syndication example

---
+++ src/cpp-winrt/syndication/main.cpp
@@ -0,0 +1,33 @@
+// Adapted from https://learn.microsoft.com/en-us/windows/uwp/cpp-and-winrt-apis/get-started
+
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Web.Syndication.h>
+
+import <iostream>;
+
+using namespace winrt;
+using namespace Windows::Foundation;
+using namespace Windows::Web::Syndication;
+
+int main()
+{
+    winrt::init_apartment();
+
+    Uri rssFeedUri{ L"https://blogs.windows.com/feed" };
+    SyndicationClient syndicationClient;
+    SyndicationFeed syndicationFeed = syndicationClient.RetrieveFeedAsync(rssFeedUri).get();
+    for (const SyndicationItem syndicationItem : syndicationFeed.Items())
+    {
+        winrt::hstring titleAsHstring = syndicationItem.Title().Text();
+
+        // A workaround to remove the trademark symbol from the title string, because it causes issues in this case.
+        std::wstring titleAsStdWstring{ titleAsHstring.c_str() };
+        titleAsStdWstring.erase(remove(titleAsStdWstring.begin(), titleAsStdWstring.end(), L'™'), titleAsStdWstring.end());
+        titleAsHstring = titleAsStdWstring;
+
+        std::wcout << titleAsHstring.c_str() << std::endl;
+    }
+
+    return 0;
+}
+
