c67107a3e71ac32471ba96ad790d2b9ac321efd1
Vasilios Magriplis <etameson@gmail.com>
2023-03-10 20:34:22 +1100

    Add random numbers to JSON sample

--- src/cpp-winrt/json/main.cpp
+++ src/cpp-winrt/json/main.cpp
@@ -3,6 +3,7 @@
 #include <winrt/windows.data.json.h>
 
 import <iostream>;
+import <random>;
 
 // https://docs.microsoft.com/en-us/uwp/api/windows.data.json?view=winrt-22621
 namespace JSON = winrt::Windows::Data::Json;
@@ -12,13 +13,15 @@ int main()
     JSON::JsonObject root;
     JSON::JsonArray arrayValue;
 
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    std::uniform_int_distribution<std::mt19937::result_type> dist6(1, 6); // distribution in range [1, 6]
+
     for (int i = 0; i < 5; i++)
     {
         JSON::JsonArray internalArray;
-
-        internalArray.Append(JSON::JsonValue::CreateNumberValue(1));
-        internalArray.Append(JSON::JsonValue::CreateNumberValue(2));
-        internalArray.Append(JSON::JsonValue::CreateNumberValue(3));
+        for (int j = 0; j < 3; j++)
+            internalArray.Append(JSON::JsonValue::CreateNumberValue(dist6(rng)));
 
         arrayValue.Append(internalArray);
     }
--- src/cpp-winrt/module-header/std.cpp
+++ src/cpp-winrt/module-header/std.cpp
@@ -1,1 +1,2 @@
 import <iostream>;
+import <random>;
