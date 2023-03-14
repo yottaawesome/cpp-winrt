f78da118833c6b698d99dec5136b77516b55d936
Vasilios Magriplis <etameson@gmail.com>
2023-03-14 20:52:49 +1100

    Complete the message-websocket-client sample.

--- src/message-websocket-server/main.cpp
+++ src/message-websocket-server/main.cpp
@@ -1,6 +1,16 @@
+#include <winrt/windows.foundation.h>
+#include <winrt/windows.foundation.collections.h>
+#include <winrt/windows.networking.sockets.h>
+
 import <iostream>;
 
 int main()
 {
+    namespace Sockets = winrt::Windows::Networking::Sockets;
+    
+    // TODO: not sure how to go about this one. It appears you 
+    // can't create an arbitrary websocket server.
+    // https://learn.microsoft.com/en-us/uwp/api/windows.networking.sockets.servermessagewebsocket?view=winrt-22621
+
     return 0;
 }
