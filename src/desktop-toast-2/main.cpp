6667bc8110af3eb7121c5ed4184cf07e3ff7d64d
Vasilios Magriplis <etameson@gmail.com>
2023-12-03 10:05:40 +1100

    Add desktop-toast-2 sample

---
+++ src/desktop-toast-2/main.cpp
@@ -0,0 +1,256 @@
+#pragma comment(lib, "onecore")
+#pragma comment(lib, "propsys")
+
+#include <shlobj.h>
+#include <propvarutil.h>
+#include <propkey.h>
+#include <notificationactivationcallback.h>
+#include <winrt/Windows.foundation.h>
+#include <winrt/Windows.UI.Notifications.h>
+#include <winrt/Windows.Data.Xml.Dom.h>
+#include <iostream>
+
+using namespace winrt;
+using namespace Windows::UI::Notifications;
+using namespace Windows::Data::Xml::Dom;
+
+static constexpr GUID callback_guid // B85B068A-169E-420F-B70E-5FEFCA71AE28
+{
+    0xB85B068A, 0x169E, 0x420F, {0xB7, 0x0E, 0x5F, 0xEF, 0xCA, 0x71, 0xAE, 0x28}
+};
+
+struct callback : winrt::implements<callback, INotificationActivationCallback>
+{
+    HRESULT __stdcall Activate(
+        [[maybe_unused]] LPCWSTR app,
+        [[maybe_unused]] LPCWSTR args,
+        [[maybe_unused]] NOTIFICATION_USER_INPUT_DATA const* data,
+        [[maybe_unused]] ULONG count) noexcept final
+    {
+        printf("\nCallback\n");
+        return S_OK;
+    }
+};
+
+struct callback_factory : implements<callback_factory, IClassFactory>
+{
+    HRESULT __stdcall CreateInstance(
+        IUnknown* outer,
+        GUID const& iid,
+        void** result) noexcept final
+    {
+        *result = nullptr;
+
+        if (outer)
+        {
+            return CLASS_E_NOAGGREGATION;
+        }
+
+        return make<callback>()->QueryInterface(iid, result);
+    }
+
+    HRESULT __stdcall LockServer(BOOL) noexcept final
+    {
+        return S_OK;
+    }
+};
+
+struct prop_variant : PROPVARIANT
+{
+    prop_variant() noexcept : PROPVARIANT{}
+    {
+    }
+
+    ~prop_variant() noexcept
+    {
+        clear();
+    }
+
+    void clear() noexcept
+    {
+        WINRT_VERIFY_(S_OK, PropVariantClear(this));
+    }
+};
+
+struct registry_traits
+{
+    using type = HKEY;
+
+    static void close(type value) noexcept
+    {
+        WINRT_VERIFY_(ERROR_SUCCESS, RegCloseKey(value));
+    }
+
+    static constexpr type invalid() noexcept
+    {
+        return nullptr;
+    }
+};
+
+using registry_key = winrt::handle_type<registry_traits>;
+
+std::wstring get_module_path()
+{
+    std::wstring path(100, L'?');
+    DWORD actual_size{};
+
+    while (true)
+    {
+        actual_size = GetModuleFileNameW(nullptr, path.data(), 1 + static_cast<uint32_t>(path.size()));
+
+        if (actual_size < 1 + path.size())
+        {
+            path.resize(actual_size);
+            break;
+        }
+        else
+        {
+            path.resize(path.size() * 2, L'?');
+        }
+    }
+
+    return path;
+}
+
+std::wstring get_shortcut_path()
+{
+    // This was the original code, but updating this path requires admin privileges,
+    // I've updated the sample to install it in the user's roaming profile.
+    //auto format = LR"(%ProgramData%\Microsoft\Windows\Start Menu\Programs\SampleApp.lnk)";
+    //auto required = ExpandEnvironmentStringsW(format, nullptr, 0);
+    //std::wstring path(required - 1, L'?');
+    //ExpandEnvironmentStringsW(format, path.data(), required);
+    //return path;
+
+    PWSTR appData = nullptr;
+    auto hr = ::SHGetKnownFolderPath(FOLDERID_RoamingAppData, 0, nullptr, &appData);
+    if (FAILED(hr))
+    {
+        CoTaskMemFree(appData);
+        throw std::runtime_error("Failed");
+    }
+    std::wstring format(appData);
+    CoTaskMemFree(appData);
+    format += L"\\SampleApp.lnk";
+    return format;
+}
+
+void create_shortcut()
+{
+    auto link = create_instance<IShellLinkW>(CLSID_ShellLink);
+    check_hresult(link->SetPath(get_module_path().c_str()));
+
+    auto store = link.as<IPropertyStore>();
+    prop_variant value;
+    check_hresult(InitPropVariantFromString(L"SampleApp", &value));
+    check_hresult(store->SetValue(PKEY_AppUserModel_ID, value));
+    value.clear();
+    check_hresult(InitPropVariantFromCLSID(callback_guid, &value));
+    check_hresult(store->SetValue(PKEY_AppUserModel_ToastActivatorCLSID, value));
+
+    auto file = store.as<IPersistFile>();
+    check_hresult(file->Save(get_shortcut_path().c_str(), TRUE));
+}
+
+void update_registry()
+{
+    std::wstring key_path{ LR"(SOFTWARE\Classes\CLSID\{????????-????-????-????-????????????})" };
+    StringFromGUID2(callback_guid, key_path.data() + 23, 39);
+    key_path += LR"(\LocalServer32)";
+    registry_key key;
+
+    check_win32(RegCreateKeyExW(
+        HKEY_CURRENT_USER,
+        key_path.c_str(),
+        0,
+        nullptr,
+        0,
+        KEY_WRITE,
+        nullptr,
+        key.put(),
+        nullptr));
+
+    auto path = get_module_path();
+
+    check_win32(RegSetValueExW(
+        key.get(),
+        nullptr,
+        0,
+        REG_SZ,
+        reinterpret_cast<BYTE const*>(path.c_str()),
+        static_cast<uint32_t>((path.size() + 1) * sizeof(wchar_t))));
+}
+
+void register_callback()
+{
+    DWORD registration{};
+
+    check_hresult(CoRegisterClassObject(
+        callback_guid,
+        make<callback_factory>().get(),
+        CLSCTX_LOCAL_SERVER,
+        REGCLS_SINGLEUSE,
+        &registration));
+}
+
+void create_toast()
+{
+    XmlDocument xml;
+
+    xml.LoadXml(
+        LR"(
+<toast>
+  <visual>
+    <binding template='ToastGeneric'>
+      <text>Sample App</text>
+    </binding>
+  </visual>
+  <actions>
+    <action content='Callback' arguments='args' activationKind='Background' />
+  </actions>
+</toast>
+)");
+
+    ToastNotification toast{ xml };
+    auto notifier = ToastNotificationManager::CreateToastNotifier(L"SampleApp");
+    toast.Activated([](const auto&, const auto&)
+        {
+            std::wcout << "ActivATED\n";
+        });
+    notifier.Show(toast);
+}
+
+int main()
+{
+    try
+    {
+        init_apartment();
+
+        //if (IsUserAnAdmin()) // Not require when installing to user's roaming profile
+        {
+            create_shortcut();
+            update_registry();
+        }
+
+        register_callback();
+        printf("\nSample app is running%s\n", IsUserAnAdmin() ? " (Administrator)" : "");
+        printf("\nPress 'T' to create toast or any other key to exit\n");
+
+        if (toupper(getchar()) == 'T')
+        {
+            create_toast();
+        }
+
+        MSG msg;
+        while (GetMessage(&msg, nullptr, 0, 0))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        //Sleep(1000);
+    }
+    catch (hresult_error const& e)
+    {
+        printf("Error: %ls (0x%8X)\n", e.message().c_str(), static_cast<uint32_t>(e.code()));
+    }
+}
---
+++ src/desktop-toasts/main.cpp
@@ -0,0 +1,213 @@
+#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#include <SDKDDKVer.h>
+#include <Windows.h>
+#include <Psapi.h>
+#include <strsafe.h>
+#include <ShObjIdl.h>
+#include <Shlobj.h>
+#include <Pathcch.h>
+#include <propvarutil.h>
+#include <propkey.h>
+#include <wrl.h>
+#include <wrl\wrappers\corewrappers.h>
+#include <windows.ui.notifications.h>
+#include "NotificationActivationCallback.h"
+#include <winrt/base.h>
+#include <winrt/windows.foundation.h>
+#include <winrt/windows.foundation.collections.h>
+#include <winrt/windows.ui.notifications.h>
+#include <winrt/windows.data.xml.dom.h>
+#include <iostream>
+
+#pragma comment(lib, "Pathcch.lib")
+
+const wchar_t AppId[] = L"Microsoft.Samples.DesktopToasts";
+
+struct CoTaskMemStringTraits
+{
+    typedef PWSTR Type;
+
+    inline static bool Close(_In_ Type h) throw() { ::CoTaskMemFree(h); return true; }
+
+    inline static Type GetInvalidValue() throw() { return nullptr; }
+};
+typedef Microsoft::WRL::Wrappers::HandleT<CoTaskMemStringTraits> CoTaskMemString;
+
+// For the app to be activated from Action Center, it needs to provide a COM server to be called
+// when the notification is activated.  The CLSID of the object needs to be registered with the
+// OS via its shortcut so that it knows who to call later.
+class DECLSPEC_UUID("23A5B06E-20BB-4E7E-A0AC-6982ED6A6041") NotificationActivator WrlSealed
+    : public Microsoft::WRL::RuntimeClass < Microsoft::WRL::RuntimeClassFlags<Microsoft::WRL::ClassicCom>,
+    INotificationActivationCallback > WrlFinal
+{
+public:
+    virtual HRESULT STDMETHODCALLTYPE Activate(
+        _In_ LPCWSTR /*appUserModelId*/,
+        _In_ LPCWSTR /*invokedArgs*/,
+        /*_In_reads_(dataCount)*/ const NOTIFICATION_USER_INPUT_DATA* /*data*/,
+        ULONG /*dataCount*/) override
+    {
+        return S_OK;// DesktopToastsApp::GetInstance()->SetMessage(L"NotificationActivator - The user clicked on the toast.");
+    }
+};
+CoCreatableClass(NotificationActivator);
+
+HRESULT InstallShortcut(PCWSTR shortcutPath, PCWSTR exePath)
+{
+    Microsoft::WRL::ComPtr<IShellLink> shellLink;
+    HRESULT hr = CoCreateInstance(CLSID_ShellLink, nullptr, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&shellLink));
+    if (FAILED(hr))
+        return hr;
+
+    hr = shellLink->SetPath(exePath);
+    if (FAILED(hr))
+        return hr;
+
+    Microsoft::WRL::ComPtr<IPropertyStore> propertyStore;
+
+    hr = shellLink.As(&propertyStore);
+    if (FAILED(hr))
+        return hr;
+
+    PROPVARIANT propVar;
+    propVar.vt = VT_LPWSTR;
+    propVar.pwszVal = const_cast<PWSTR>(AppId); // for _In_ scenarios, we don't need a copy
+    hr = propertyStore->SetValue(PKEY_AppUserModel_ID, propVar);
+    if (FAILED(hr))
+        return hr;
+
+    propVar.vt = VT_CLSID;
+    propVar.puuid = const_cast<CLSID*>(&__uuidof(NotificationActivator));
+    hr = propertyStore->SetValue(PKEY_AppUserModel_ToastActivatorCLSID, propVar);
+    if (FAILED(hr))
+        return hr;
+
+    hr = propertyStore->Commit();
+    if (FAILED(hr))
+        return hr;
+
+    Microsoft::WRL::ComPtr<IPersistFile> persistFile;
+    hr = shellLink.As(&persistFile);
+    if (FAILED(hr))
+        return hr;
+
+    hr = persistFile->Save(shortcutPath, TRUE);
+
+    return hr;
+}
+
+HRESULT RegisterComServer(PCWSTR exePath)
+{
+    // We don't need to worry about overflow here as ::GetModuleFileName won't
+    // return anything bigger than the max file system path (much fewer than max of DWORD).
+    DWORD dataSize = static_cast<DWORD>((::wcslen(exePath) + 1) * sizeof(WCHAR));
+
+    // In this sample, the app UI is registered to launch when the COM callback is needed.
+    // Other options might be to launch a background process instead that then decides to launch
+    // the UI if needed by that particular notification.
+    return HRESULT_FROM_WIN32(::RegSetKeyValue(
+        HKEY_CURRENT_USER,
+        LR"(SOFTWARE\Classes\CLSID\{23A5B06E-20BB-4E7E-A0AC-6982ED6A6041}\LocalServer32)",
+        nullptr,
+        REG_SZ,
+        reinterpret_cast<const BYTE*>(exePath),
+        dataSize));
+}
+
+HRESULT RegisterAppForNotificationSupport()
+{
+    CoTaskMemString appData;
+    auto hr = ::SHGetKnownFolderPath(FOLDERID_RoamingAppData, 0, nullptr, appData.GetAddressOf());
+    if (FAILED(hr))
+        return hr;
+
+    wchar_t shortcutPath[MAX_PATH];
+    hr = ::PathCchCombine(shortcutPath, ARRAYSIZE(shortcutPath), appData.Get(), LR"(Microsoft\Windows\Start Menu\Programs\winrt-desktop-toasts.lnk)");
+    if (FAILED(hr))
+        return hr;
+
+    DWORD attributes = ::GetFileAttributes(shortcutPath);
+    bool fileExists = attributes < 0xFFFFFFF;
+    if (fileExists)
+        return hr;
+
+    wchar_t exePath[MAX_PATH];
+    DWORD charWritten = ::GetModuleFileName(nullptr, exePath, ARRAYSIZE(exePath));
+    hr = charWritten > 0 ? S_OK : HRESULT_FROM_WIN32(::GetLastError());
+    if (FAILED(hr))
+        return hr;
+
+    hr = InstallShortcut(shortcutPath, exePath);
+    if (FAILED(hr))
+        return hr;
+
+    hr = RegisterComServer(exePath);
+
+    return hr;
+}
+
+HRESULT RegisterActivator()
+{
+    // Module<OutOfProc> needs a callback registered before it can be used.
+    // Since we don't care about when it shuts down, we'll pass an empty lambda here.
+    Microsoft::WRL::Module<Microsoft::WRL::OutOfProc>::Create([] {});
+
+    // If a local server process only hosts the COM object then COM expects
+    // the COM server host to shutdown when the references drop to zero.
+    // Since the user might still be using the program after activating the notification,
+    // we don't want to shutdown immediately.  Incrementing the object count tells COM that
+    // we aren't done yet.
+    Microsoft::WRL::Module<Microsoft::WRL::OutOfProc>::GetModule().IncrementObjectCount();
+
+    return Microsoft::WRL::Module<Microsoft::WRL::OutOfProc>::GetModule().RegisterObjects();
+}
+
+int main()
+{
+    try
+    {
+        winrt::init_apartment();
+        RegisterAppForNotificationSupport();
+        RegisterActivator();
+
+        winrt::Windows::Data::Xml::Dom::XmlDocument doc;
+        doc.LoadXml(L"<toast></toast>");
+        winrt::Windows::UI::Notifications::ToastNotification toast(doc);
+        toast.Failed(
+            [](const winrt::Windows::UI::Notifications::ToastNotification& s, const winrt::Windows::UI::Notifications::ToastFailedEventArgs& t)
+            {
+                std::wcout << L"Failed!" << std::endl;
+            }
+        );
+
+        toast.Activated(
+            [](const auto& s, const auto&)
+            {
+                std::wcout << L"I'm activated!" << std::endl;
+            }
+        );
+
+        toast.Dismissed(
+            [](const winrt::Windows::UI::Notifications::ToastNotification& s, const winrt::Windows::UI::Notifications::ToastDismissedEventArgs& t)
+            {
+                std::wcout << L"I'm dismissed!" << std::endl;
+            });
+        winrt::Windows::UI::Notifications::ToastNotifier notifier = winrt::Windows::UI::Notifications::ToastNotificationManager::CreateToastNotifier(AppId);
+        notifier.Show(toast);
+
+        MSG msg;
+        while (GetMessage(&msg, nullptr, 0, 0))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+    catch (const winrt::hresult_error& ex)
+    {
+        std::wcerr << ex.message().c_str() << std::endl;
+    }
+
+    return 0;
+}
+
